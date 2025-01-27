44501f856aa7de10c68f149a2b283efd9a584617
Artur Castro <arturcastro010@gmail.com>
2025-01-26 23:31:19 -0300

    Movimentação de jogos na lista finalizada

--- dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,8 @@ public class GameListService {
 
     @Autowired //"Injeta" o repositório
     private GameListRepository gameListRepository;
+    @Autowired
+    private GameRepository gameRepository;
 
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
@@ -22,6 +25,29 @@ public class GameListService {
         List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 
         return dto;
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        // Recupera a lista de jogos na ordem correta
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        // Remove o objeto da posição original
+        GameMinProjection obj = list.remove(sourceIndex);
+
+        // Adiciona o objeto na nova posição
+        list.add(destinationIndex, obj);
+
+        // Determina o intervalo de atualização
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        // Atualiza as posições no banco de dados
+        for (int i = min; i <= max; i++) {
+            GameMinProjection game = list.get(i);
+            gameListRepository.updateBelongingPosition(listId, game.getId(), i);
+        }
     }
 
 }
